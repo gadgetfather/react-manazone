cf0ca0d007ff39726b963adc513100310cf6385e
Akshay Kamble <kamblea635@gmail.com>
2022-03-07 19:21:19 +0530

    create product card

---
+++ src/components/ProductCard.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+export default function ProductCard() {
+  return (
+    <div class="card-container product-card">
+      <div class="img-container">
+        <img class="card-img" src="https://picsum.photos/200/200" alt="..." />
+      </div>
+      <span class="fav-icon">
+        <i class="far fa-heart"></i>
+      </span>
+      <h1 class="card-title">Product title</h1>
+      <h2 class="card-subtitle">price</h2>
+      <a class="btn btn-primary" href="#">
+        add to cart
+      </a>
+    </div>
+  );
+}
