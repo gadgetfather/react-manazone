2816a7ba223570cf6b547c331745a03366a0a8ac
Akshay Kamble <kamblea635@gmail.com>
2022-03-08 12:47:52 +0530

    Created hero component

---
+++ src/components/HeroSection.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import heroImg from "../assets/images/hero-banner.jfif";
+
+export default function HeroSection() {
+  return (
+    <div class="hero-img">
+      <img src={heroImg} alt="hero-banner" />
+      <div class="card-container card-vertical card-shadow">
+        <h1 class="card-title">Welcome to Manazone</h1>
+        <p>Explore the gaming world</p>
+        <a class="btn btn-primary" href="/product-page/product.html">
+          Shop now <i class="fas fa-chevron-right"></i>
+        </a>
+      </div>
+    </div>
+  );
+}
