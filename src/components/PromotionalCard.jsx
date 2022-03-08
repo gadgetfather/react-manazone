a5c2c13854fc984d5b33abb0d6ebb41cb710e801
Akshay Kamble <kamblea635@gmail.com>
2022-03-08 12:49:05 +0530

    created Promotional card css

---
+++ src/components/PromotionalCard.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+export default function PromotionalCard() {
+  return (
+    <div class="card-container card-vertical card-shadow">
+      <div class="product-img-box">
+        <img
+          class="card-img"
+          src="https://picsum.photos/200"
+          alt="promotional"
+        />
+      </div>
+      <h1 class="card-title">Farcry6</h1>
+      <p class="card-description">
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quas, et,
+        optio quaerat nam reprehenderit quisquam nihil
+      </p>
+      <a href="#">
+        Know More <i class="fas fa-chevron-right"></i>
+      </a>
+    </div>
+  );
+}
