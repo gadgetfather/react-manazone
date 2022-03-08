cd657a0deacd05081fbef5508af31b8289e3e4aa
Akshay Kamble <kamblea635@gmail.com>
2022-03-08 12:47:33 +0530

    Created ProductPage

---
+++ src/pages/product/ProductPage.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Aside from "../../components/Aside";
+import ProductGrid from "../../components/ProductGrid";
+
+export default function ProductPage() {
+  return (
+    <main class="main-content_product">
+      <Aside />
+      <ProductGrid />
+    </main>
+  );
+}
