94cd05537c29da3198cb9193f5b9c4f711b8e8b8
Akshay Kamble <kamblea635@gmail.com>
2022-03-11 16:03:31 +0530

    added custom router

--- src/index.js
+++ src/index.js
@@ -3,12 +3,15 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import { ThemeProvider } from "./context/themeContext";
+import { RouterProvider } from "./context/routerContext";
 
 ReactDOM.render(
   <React.StrictMode>
-    <ThemeProvider>
-      <App />
-    </ThemeProvider>
+    <RouterProvider>
+      <ThemeProvider>
+        <App />
+      </ThemeProvider>
+    </RouterProvider>
   </React.StrictMode>,
   document.getElementById("root")
 );
